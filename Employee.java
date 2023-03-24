7e2a8c015d55f71fff6d3618a2f2cf3d93c3a5bb
22C006 <22c006@gectcr.ac.in>
2023-03-24 12:14:40 +0530

    fourth commit

---
+++ Employee.java
@@ -0,0 +1,79 @@
+package cycle2;
+
+import java.util.*;
+
+class Employee {
+
+	int eNo;
+
+	String eName;
+
+	int eSalary;
+
+	public void read() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter ID : ");
+
+		eNo = Integer.parseInt(sc.nextLine());
+
+		System.out.print("Enter Name : ");
+
+		eName = sc.nextLine();
+
+		System.out.print("Enter monthly salary : ");
+
+		eSalary = Integer.parseInt(sc.nextLine());
+
+	}
+
+	public String toString() {
+
+		return "Name : " + eName +" Salary : "+eSalary;
+
+	}
+
+	public static void main(String[] args) {
+
+		int i, n = 3;
+
+		int No;
+
+		Employee emp[] = new Employee[n];
+
+		for (i = 0; i < n; i++) {
+
+			emp[i] = new Employee();
+
+			emp[i].read();
+
+		}
+
+		System.out.println("Search");
+
+		while (true) {
+
+			Scanner sc = new Scanner(System.in);
+
+			System.out.print("Enter ID : ");
+
+			No = Integer.parseInt(sc.nextLine());
+
+			for (i = 0; i < n; i++) {
+
+				if (emp[i].eNo == No) {
+
+					System.out.println(emp[i]);
+
+					break;
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
